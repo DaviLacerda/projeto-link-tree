ba6bd40b2bb33132d415d833c6b9d075a845bf3e
Davi Lacerda <dlacerda9@gmail.com>
2021-11-12 14:04:38 -0300

    primeiros passos em js

--- script/login.js
+++ script/login.js
@@ -1,16 +1,33 @@
-var button = document.getElementsByClassName('login-button')[0];
+var button = document.getElementById('login-btn');
 
-button.addEventListener("click", function(e){
-
+button.addEventListener("click", (e) => {
     e.preventDefault();
+    e.stopPropagation()
 
-    const userInput = document.getElementById('username')
+    const userEmail = document.getElementById('email')
     const userPassword = document.getElementById('password')
 
-    const username = userInput.value;
+    const email = userEmail.value;
     const password = userPassword.value;
 
-    console.log(username,password);
+    verifyEmail(email);
+    verifyPassword(password);
 })
 
-//rodando o script duas vezes! ?wtf
+function verifyEmail(email) {
+    let filter = /^([a-zA-Z0-9_\.\-])+\@(([a-zA-Z0-9\-])+\.)+([a-zA-Z0-9]{2,3})$/;
+
+    if(!filter.test(email)){
+        alert('Por favor, digite um email válido!\n');
+        return false;
+    }
+}
+
+function verifyPassword (password) {
+    let filter = /^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9]).{6,20}$/;
+
+    if(!filter.test(password)){
+        alert('Por favor, digite uma senha válida!\n');
+        return false;
+    }
+}
